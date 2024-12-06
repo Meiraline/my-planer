7202c86e74fbcf506601d3583ff05e21d7834577
Mairaline <ma.sarycheva2005@gmail.com>
2024-12-06 16:11:13 +0700

    базовая вёрстка версия 1

--- src/App.js
+++ src/App.js
@@ -38,14 +38,16 @@ function TopMenu(){
 function MenuBottom(){
   return(
     <div className='MenuBottom'>
-<div>
+      <div>
       <button>1</button>
       <button>2</button>
       <button>3</button>
       <button>4</button>
     </div>
-
+   <div>
     <button>+</button>
+    </div>
+    
     </div>
   )
 }
